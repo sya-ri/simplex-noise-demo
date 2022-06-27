5e4a5cdd6ec5eadca6f5788d8eb3fc6096d44a3d
sya-ri <git@s7a.dev>
2022-06-27 09:44:40 +0900

    chore: Minify index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true
   },
-  plugins: [react()]
+  plugins: [react(), createHtmlPlugin({ minify: true })]
 })
