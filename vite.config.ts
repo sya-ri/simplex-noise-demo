9efa168e51f1947e372265741f7b338b712dab43
sya-ri <git@s7a.dev>
2022-06-27 09:43:50 +0900

    chore: Move index.html to src

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   plugins: [react()]
 })
