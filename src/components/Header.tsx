080fa0f4ce5f55f2b1e3af521c7e9d581df14e9d
sya-ri <git@s7a.dev>
2022-06-27 11:36:09 +0900

    feat: Add settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
 import React, { FC } from 'react'
-import { Box, Container, Flex, Heading } from '@chakra-ui/react'
+import { Box, Container, Flex, Heading, Icon, IconButton, useDisclosure } from '@chakra-ui/react'
+import { FiSettings } from 'react-icons/fi'
+import SettingsDrawer from './SettingsDrawer'
 
 const Header: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <Box bg="teal.400" py={2} boxShadow="lg">
+    <Box bg="teal.500" py={2} boxShadow="lg">
       <Container>
-        <Flex justify="center">
-          <Heading color="gray.800">simplex-noise</Heading>
+        <Flex justify="space-between" m="auto">
+          <Heading fontSize={36} color="gray.800">
+            simplex-noise
+          </Heading>
+          <IconButton aria-label="Open Settings" colorScheme="teal" color="gray.800" rounded="full" onClick={onOpen}>
+            <Icon as={FiSettings} w={6} h={6} />
+          </IconButton>
         </Flex>
       </Container>
+      <SettingsDrawer isOpen={isOpen} onClose={onClose} />
     </Box>
   )
 }
---
+++ src/components/SettingsDrawer.tsx
@@ -0,0 +1,146 @@
+import React, { FC, useState } from 'react'
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormLabel,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack
+} from '@chakra-ui/react'
+import { useRecoilState } from 'recoil'
+import * as states from '../states'
+
+export type SettingsDrawerProps = {
+  isOpen: boolean
+  onClose: () => void
+}
+
+const SettingsDrawer: FC<SettingsDrawerProps> = ({ isOpen, onClose }) => {
+  const [lastOptions, applyOptions] = useRecoilState(states.previewOptions)
+  const [options, setOptions] = useState(lastOptions)
+  const close = () => {
+    setOptions(lastOptions)
+    onClose()
+  }
+  return (
+    <Drawer isOpen={isOpen} placement="right" onClose={close}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Settings</DrawerHeader>
+
+        <DrawerBody>
+          <Stack spacing="24px">
+            <Box>
+              <FormLabel htmlFor="width">Width</FormLabel>
+              <NumberInput
+                id="width"
+                defaultValue={options.width}
+                min={1}
+                onChange={(_value, value) => setOptions({ ...options, width: value })}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+            <Box>
+              <FormLabel htmlFor="height">Height</FormLabel>
+              <NumberInput
+                id="height"
+                defaultValue={options.height}
+                min={1}
+                onChange={(_value, value) => setOptions({ ...options, height: value })}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+            <Box>
+              <FormLabel htmlFor="stepX">Step X</FormLabel>
+              <NumberInput
+                id="stepX"
+                defaultValue={options.stepX}
+                onChange={(_value, value) => setOptions({ ...options, stepX: value })}
+              >
+                <NumberInputField />
+              </NumberInput>
+            </Box>
+            <Box>
+              <FormLabel htmlFor="stepX">Step Y</FormLabel>
+              <NumberInput
+                id="stepY"
+                defaultValue={options.stepY}
+                onChange={(_value, value) => setOptions({ ...options, stepY: value })}
+              >
+                <NumberInputField />
+              </NumberInput>
+            </Box>
+            <Box>
+              <FormLabel htmlFor="cellWidth">Cell Width</FormLabel>
+              <NumberInput
+                id="cellWidth"
+                defaultValue={options.cellWidth}
+                min={1}
+                onChange={(_value, value) => setOptions({ ...options, cellWidth: value })}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+            <Box>
+              <FormLabel htmlFor="cellHeight">Cell Height</FormLabel>
+              <NumberInput
+                id="cellHeight"
+                defaultValue={options.cellHeight}
+                min={1}
+                onChange={(_value, value) => setOptions({ ...options, cellHeight: value })}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+          </Stack>
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Button variant="outline" mr={3} onClick={close}>
+            Cancel
+          </Button>
+          <Button
+            colorScheme="blue"
+            onClick={() => {
+              applyOptions(options)
+              onClose()
+            }}
+          >
+            Apply
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default SettingsDrawer
