093b0865eda2a4073e79d4fd802e2e9207a4b344
sya-ri <git@s7a.dev>
2022-06-27 12:01:05 +0900

    feat: Add regenerate button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react'
-import { Box, Container, Flex, Heading, Icon, IconButton, Tooltip, useDisclosure } from '@chakra-ui/react'
-import { FiSettings } from 'react-icons/fi'
+import { Box, Container, Flex, Heading, HStack, Icon, IconButton, Tooltip, useDisclosure } from '@chakra-ui/react'
+import { FiSettings, FiRotateCcw } from 'react-icons/fi'
+import { useRecoilState } from 'recoil'
+import SimplexNoise from 'simplex-noise'
 import SettingsDrawer from './SettingsDrawer'
+import * as states from '../states'
 
 const Header: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const [lastOptions, applyOptions] = useRecoilState(states.previewOptions)
   return (
     <Box bg="teal.500" py={2} boxShadow="lg">
       <Container>
@@ -12,11 +16,32 @@ const Header: FC = () => {
           <Heading fontSize={32} color="gray.800">
             simplex-noise
           </Heading>
-          <Tooltip label="Open Settings">
-            <IconButton aria-label="Open Settings" colorScheme="teal" color="gray.800" rounded="full" onClick={onOpen}>
-              <Icon as={FiSettings} w={6} h={6} />
-            </IconButton>
-          </Tooltip>
+          <HStack gap={2}>
+            <Tooltip label="Regenerate">
+              <IconButton
+                aria-label="Regenerate"
+                colorScheme="teal"
+                color="gray.800"
+                rounded="full"
+                onClick={() => {
+                  applyOptions({ ...lastOptions, simplex: new SimplexNoise() })
+                }}
+              >
+                <Icon as={FiRotateCcw} w={6} h={6} />
+              </IconButton>
+            </Tooltip>
+            <Tooltip label="Open Settings">
+              <IconButton
+                aria-label="Open Settings"
+                colorScheme="teal"
+                color="gray.800"
+                rounded="full"
+                onClick={onOpen}
+              >
+                <Icon as={FiSettings} w={6} h={6} />
+              </IconButton>
+            </Tooltip>
+          </HStack>
         </Flex>
       </Container>
       <SettingsDrawer isOpen={isOpen} onClose={onClose} />
