aff3afd400149b4706b31bb9d80eb046a2222608
sya-ri <git@s7a.dev>
2022-06-27 11:56:14 +0900

    tweak: Add tooltip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Box, Container, Flex, Heading, Icon, IconButton, useDisclosure } from '@chakra-ui/react'
+import { Box, Container, Flex, Heading, Icon, IconButton, Tooltip, useDisclosure } from '@chakra-ui/react'
 import { FiSettings } from 'react-icons/fi'
 import SettingsDrawer from './SettingsDrawer'
 
@@ -12,9 +12,11 @@ const Header: FC = () => {
           <Heading fontSize={32} color="gray.800">
             simplex-noise
           </Heading>
-          <IconButton aria-label="Open Settings" colorScheme="teal" color="gray.800" rounded="full" onClick={onOpen}>
-            <Icon as={FiSettings} w={6} h={6} />
-          </IconButton>
+          <Tooltip label="Open Settings">
+            <IconButton aria-label="Open Settings" colorScheme="teal" color="gray.800" rounded="full" onClick={onOpen}>
+              <Icon as={FiSettings} w={6} h={6} />
+            </IconButton>
+          </Tooltip>
         </Flex>
       </Container>
       <SettingsDrawer isOpen={isOpen} onClose={onClose} />
